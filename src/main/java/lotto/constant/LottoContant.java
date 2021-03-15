4a33074c2120790f20f69e57ed778276c6472621
pjhsk113 <pjhsk113@naver.com>
2021-03-15 21:25:47 +0900

    [feat] 로또 상수 클래스 추가

---
+++ src/main/java/lotto/constant/LottoContant.java
@@ -0,0 +1,9 @@
+package lotto.constant;
+
+public class LottoContant {
+    private LottoContant() { }
+
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+
+}
