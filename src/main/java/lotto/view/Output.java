97a428a1494d02ce8f98a35739cd49228c11fa5c
pjhsk113 <pjhsk113@naver.com>
2021-03-19 22:53:20 +0900

    [feat] Output 클래스 기능 추가
    - 구매한 로또 내역 출력하는 로직 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,26 @@
 package lotto.view;
 
-public class Output {
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 
+import java.util.stream.Collectors;
+
+public class Output {
+    private static final String BUYING_FORMAT = "%d개를 구매했습니다.\n%s\n\n";
+
+    private Output() { }
+
+    public static void print(Lottos lottos) {
+        String lottoList = lottos.stream()
+                .map(Output::getLotto)
+                .collect(Collectors.joining("\n"));
+        System.out.printf(BUYING_FORMAT, lottos.getLottoCount(), lottoList);
+    }
+
+    private static String getLotto(Lotto lotto) {
+        String str = lotto.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+        return "[" + str + "]";
+    }
 }
