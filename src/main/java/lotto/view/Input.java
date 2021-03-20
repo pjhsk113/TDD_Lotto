9637ebd6b3e7d7f0026ef814529f06a1608ae68a
pjhsk113 <pjhsk113@naver.com>
2021-03-20 16:49:21 +0900

    [feat] 당첨 번호 입력 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Input {
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
+    private static final String ENTER_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요. ex) 1, 2, 3, 4, 5, 6";
     private static final Scanner sc = new Scanner(System.in);
 
     public static int enterAmount() {
         System.out.println(ENTER_PURCHASE_AMOUNT_MESSAGE);
         return sc.nextInt();
+    }
+
+    public static String enterWinningNumber() {
+        System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
+        return sc.nextLine();
     }
 }
