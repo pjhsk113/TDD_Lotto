4c4da7bee0ebb14c7b0b681d520bca14a6d6d2d1
pjhsk113 <pjhsk113@naver.com>
2021-03-21 17:00:06 +0900

    [feat] 당첨 통계 테스트 추가

---
+++ src/test/java/lotto/domain/lottoResult/WinCountTest.java
@@ -0,0 +1,19 @@
+package lotto.domain.lottoResult;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinCountTest {
+
+    @DisplayName("상금 계산이 올바른지 테스트")
+    @Test
+    void reward_Test() {
+        WinCount winCount = new WinCount(5);
+        int result = winCount.moneyCount(3000);
+
+        assertThat(result).isEqualTo(15000);
+    }
+
+}
