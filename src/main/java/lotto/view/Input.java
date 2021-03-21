00c61437982cc082e8c69d5e43cc8cf43cc0d141
pjhsk113 <pjhsk113@naver.com>
2021-03-21 16:54:41 +0900

    [fix] 당첨번호 입력 수정
    -  Scanner의 nextLine이 공백을 입력받는 문제 수정

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Input {
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
-    private static final String ENTER_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요. ex) 1, 2, 3, 4, 5, 6";
+    private static final String ENTER_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final Scanner sc = new Scanner(System.in);
 
     public static int enterAmount() {
@@ -14,6 +14,7 @@ public class Input {
 
     public static String enterWinningNumber() {
         System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
+        sc.nextLine();
         return sc.nextLine();
     }
 }
