97769adacc7270e4a1833319e7ea46846861d0cb
pjhsk113 <pjhsk113@naver.com>
2021-03-21 16:56:24 +0900

    [feat] 당첨 통계 계산 역할을 가지는 Result 클래스 생성

---
+++ src/main/java/lotto/domain/lottoResult/LottoResults.java
@@ -0,0 +1,30 @@
+package lotto.domain.lottoResult;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoResults {
+    private final List<LottoResult> lottoResults;
+
+    public LottoResults(List<LottoResult> lottoResults) {
+        this.lottoResults = lottoResults;
+    }
+
+    public static LottoResults of() {
+        return new LottoResults(Arrays.stream(Rank.values())
+                .map(LottoResult::of)
+                .collect(Collectors.toList()));
+    }
+
+    public void win(int hitCount) {
+        for (LottoResult result : lottoResults) {
+            result.win(hitCount);
+        }
+    }
+
+    public List<LottoResult> getLottoResults() {
+        return Collections.unmodifiableList(this.lottoResults);
+    }
+}
