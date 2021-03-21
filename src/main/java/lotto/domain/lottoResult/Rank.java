46f4b100b5a3aed4fad826c6a006585c19580ee3
pjhsk113 <pjhsk113@naver.com>
2021-03-21 16:57:44 +0900

    [feat] 당첨 순위와 당첨 금액을 담당하는 Rank 생성

---
+++ src/main/java/lotto/domain/lottoResult/Rank.java
@@ -0,0 +1,34 @@
+package lotto.domain.lottoResult;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    LOSE(0, 0);
+
+    private int hit;
+    private int reward;
+
+    Rank(int hit, int reward) {
+        this.hit = hit;
+        this.reward = reward;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public static Rank valueOf(int hit) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.getHit() == hit)
+                .findFirst()
+                .orElse(LOSE);
+    }
+}
