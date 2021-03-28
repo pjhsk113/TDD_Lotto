77db0e1f361804b52ec09dee8617635cc4691dbf
pjhsk113 <pjhsk113@naver.com>
2021-03-28 15:56:56 +0900

    [feat] 보너스볼 입력 추가

--- src/main/java/lotto/step2/view/Input.java
+++ src/main/java/lotto/step2/view/Input.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Input {
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
     private static final String ENTER_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_BALL_MAESSAGE = "보너스 볼을 입력해 주세요.";
+
     private static final Scanner sc = new Scanner(System.in);
 
     public static int enterAmount() {
@@ -16,5 +18,10 @@ public class Input {
         System.out.println(ENTER_WINNING_NUMBER_MESSAGE);
         sc.nextLine();
         return sc.nextLine();
+    }
+
+    public static int enterBonusNumber() {
+        System.out.println(ENTER_BONUS_BALL_MAESSAGE);
+        return sc.nextInt();
     }
 }
