356492f58006d5ff6b750cb6e48e4db15ca3377c
pjhsk113 <pjhsk113@naver.com>
2021-03-28 16:25:54 +0900

    [feat] Rank 2등 변경(보너스볼 당첨)
    - 당첨 개수가 5개일 때 filter 추가

--- src/main/java/lotto/step2/domain/lottoResult/Rank.java
+++ src/main/java/lotto/step2/domain/lottoResult/Rank.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     LOSE(0, 0);
 
     private int hit;
@@ -25,9 +26,10 @@ public enum Rank {
         return reward;
     }
 
-    public static Rank valueOf(int hit) {
+    public static Rank valueOf(int hit, boolean hasBonusNumber) {
         return Arrays.stream(values())
                 .filter(rank -> rank.getHit() == hit)
+                .filter(rank -> !rank.equals(SECOND) || hasBonusNumber)
                 .findFirst()
                 .orElse(LOSE);
     }
