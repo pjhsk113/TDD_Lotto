77567b337b684d2c3d7c5fe3e7140349b0bd6c72
pjhsk113 <pjhsk113@naver.com>
2021-06-06 13:47:32 +0900

    [feat] 로또 수동 구매 입력 로직 추가

--- src/main/java/lotto/step2/view/Input.java
+++ src/main/java/lotto/step2/view/Input.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Input {
     private static final String ENTER_PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
+    private static final String ENTER_MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String ENTER_MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String ENTER_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String ENTER_BONUS_BALL_MAESSAGE = "보너스 볼을 입력해 주세요.";
 
@@ -12,6 +14,17 @@ public class Input {
     public static int enterAmount() {
         System.out.println(ENTER_PURCHASE_AMOUNT_MESSAGE);
         return sc.nextInt();
+    }
+
+    public static int enterManualNumberCount() {
+        System.out.println(ENTER_MANUAL_LOTTO_COUNT_MESSAGE);
+        return sc.nextInt();
+    }
+
+    public static String enterManualNumber() {
+        System.out.println(ENTER_MANUAL_LOTTO_NUMBER_MESSAGE);
+        sc.nextLine();
+        return sc.nextLine();
     }
 
     public static String enterWinningNumber() {
