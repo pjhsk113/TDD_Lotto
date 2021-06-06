fa1f265c73406b47b56bd2566e105870970becfb
pjhsk113 <pjhsk113@naver.com>
2021-06-06 14:42:46 +0900

    [feat] LottoShop -> 수동 로또 구매 로직 추가
    - 자동 구매 로직을 별도의 private 메서드로 분리
    - buyLotto는 입력받은 수동 로또 번호에 자동 로또를 합쳐서 반환하도록 변경

--- src/main/java/lotto/step3/domain/LottoShop.java
+++ src/main/java/lotto/step3/domain/LottoShop.java
@@ -1,5 +1,6 @@
 package lotto.step3.domain;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,11 +10,17 @@ public class LottoShop {
     private static final int THOUSAND = 1000;
     private static final String LACK_MONEY = "금액이 부족합니다.";
 
-    public static Lottos buyLotto(int payment) {
+    public static Lottos buyLotto(int payment, List<Lotto> lottos) {
         validatePayment(payment);
+        int remainingPayment = payment - lottos.size() * THOUSAND;
+        lottos.addAll(buyAutoLotto(remainingPayment));
+        return Lottos.of(lottos);
+    }
+
+    private static List<Lotto> buyAutoLotto(int autoLottoPurchasePayment) {
         return Stream.generate(LottoNumberGenerator::generateLottoNumber)
-                .limit(payment / THOUSAND)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), Lottos::of));
+                .limit(autoLottoPurchasePayment / THOUSAND)
+                .collect((Collectors.toList()));
     }
 
     private static void validatePayment(int payment) {
